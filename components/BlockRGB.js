cea13e7099c6d0ae419b13731d01c1430b5c42ac
Haris Samingan <muhamad_haris@hotmail.com>
2022-10-28 21:52:37 +0800

    Add block rgb component

---
+++ components/BlockRGB.js
@@ -0,0 +1,17 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+export default function BlockRGB(props) {
+  const red = props.red;
+  const green = props.green;
+  const blue = props.blue;
+  return (
+    <View
+      style={{
+        backgroundColor: `rgb( ${red}, ${green}, ${blue} )`,
+        padding: 30,
+        width: "100%",
+      }}
+    ></View>
+  );
+}
